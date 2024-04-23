f39e7ccd0504477fa0bb2ad11f9325545ba9082f
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-04-23 05:52:38 -0700

    create helper functions

---
+++ src/functions.js
@@ -0,0 +1,33 @@
+// Helper functions
+
+// returns a visual representation of a tree
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+// returns a sorted array and deletes any duplicate values
+function sortArray(array) {
+  const sorted = [...array].sort((a, b) => {
+    return a - b;
+  });
+  let prevElement;
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] === prevElement) {
+      sorted.splice(i - 1, 1);
+      i--;
+    }
+    prevElement = sorted[i];
+  }
+  return sorted;
+}
+
+export { prettyPrint, sortArray };
