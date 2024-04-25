ac61b82d0b805d7174afa81f4ec304d75529f7c0
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-04-25 04:37:50 -0700

    create traversal functions

--- src/bst.js
+++ src/bst.js
@@ -77,7 +77,105 @@ function BST(array) {
 
     return root;
   }
-  return { root, insertNode, deleteNode };
+
+  function find(value, root) {
+    if (root === null) return null;
+
+    if (value === root.data) {
+      return root;
+    } else {
+      return value < root.data
+        ? find(value, root.left)
+        : find(value, root.right);
+    }
+  }
+
+  function levelOrder(root, callBack) {
+    const queue = [];
+    const levelOrderList = [];
+
+    if (root === null) return null;
+    queue.push(root);
+    while (queue.length !== 0) {
+      const currentNode = queue.shift();
+
+      if (currentNode.left !== null) queue.push(currentNode.left);
+      if (currentNode.right !== null) queue.push(currentNode.right);
+      callBack ? callBack(currentNode) : levelOrderList.push(currentNode);
+    }
+    if (levelOrderList.length > 0) return levelOrderList;
+  }
+
+  function levelOrder2(callBack) {
+    // traverse the tree in breadth-first level order and provide each node as an argument to the callback
+    const queue = [root];
+    const levelOrderList = [];
+    while (queue.length !== 0) {
+      const currentNode = queue.shift();
+
+      callBack ? callBack(currentNode) : levelOrderList.push(currentNode);
+
+      const enqueue = [currentNode?.left, currentNode?.right].filter(
+        (value) => value
+      );
+
+      queue.push(...enqueue);
+    }
+    if (levelOrderList.length > 0) return levelOrderList;
+  }
+
+  function inOrder(root, callBack, inOrderList = []) {
+    // L D R
+    if (root === null) return null;
+
+    inOrder(root.left, callBack, inOrderList);
+
+    callBack ? callBack(root) : inOrderList.push(root.data);
+
+    inOrder(root.right, callBack, inOrderList);
+
+    if (inOrderList.length > 0) return inOrderList;
+  }
+
+  function preOrder(root, callBack, preOrderList = []) {
+    // D L R
+
+    if (root === null) return null;
+
+    callBack ? callBack(root) : preOrderList.push(root.data);
+
+    preOrder(root.left, callBack, preOrderList);
+
+    preOrder(root.right, callBack, preOrderList);
+
+    if (preOrderList.length > 0) return preOrderList;
+  }
+
+  function postOrder(root, callBack, postOrderList = []) {
+    // L R D
+
+    if (root === null) return null;
+
+    postOrder(root.left, callBack, postOrderList);
+
+    postOrder(root.right, callBack, postOrderList);
+
+    callBack ? callBack(root) : postOrderList.push(root.data);
+
+    if (postOrderList.length > 0) return postOrderList;
+  }
+
+  return {
+    root,
+    insertNode,
+    deleteNode,
+    find,
+    levelOrder,
+    levelOrderRecursion,
+    inOrder,
+    preOrder,
+    postOrder,
+  };
 }
 
 export default BST;
