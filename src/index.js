ecd25a0870a6d61d571f84b41e4a818491d3e032
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-04-26 06:33:48 -0700

    import driver script

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import "./styles/fonts.css";
 import webpack_img from "./assets/images/webpack_img.webp";
 import BST from "./bst.js";
 import { prettyPrint, sortArray } from "./functions.js";
-
+import script from "./driver.js";
 const content = document.querySelector("#content");
 // content.textContent = "Hello Webpack";
-console.log("hello?");
-console.log(content);
+// console.log("hello?");
+// console.log(content);
 const title = document.createElement("h1");
 title.textContent = "Webpack";
 title.classList.add("greeting");
@@ -15,31 +15,56 @@ content.appendChild(title);
 const myIcon = new Image();
 myIcon.src = webpack_img;
 content.appendChild(myIcon);
-const secondArray = [30, 40, 20, 32, 34, 36, 50, 60, 65, 70, 80, 85, 75];
-// const unsortedArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const sortedArray = sortArray(secondArray);
-console.log(sortedArray);
 
-const Tree = BST(sortedArray);
-// Tree.insertNode(6, Tree.root);
-console.log(Tree.root);
-Tree.insertNode(82, Tree.root);
+script();
+// const secondArray = [30, 40, 20, 31, 34, 36, 50, 60, 65, 70, 80, 85, 75];
+// // const unsortedArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+// const sortedArray = sortArray(secondArray);
+// console.log(sortedArray);
+
+// const Tree = BST(sortedArray);
+// // Tree.insertNode(6, Tree.root);
+// console.log(Tree.root);
+// Tree.insertNode(82, Tree.root);
 // Tree.insertNode(81, Tree.root);
-Tree.insertNode(83, Tree.root);
-Tree.insertNode(84, Tree.root);
-console.log(prettyPrint(Tree.root));
-// Tree.deleteNode(50, Tree.root);
+// // Tree.insertNode(83, Tree.root);
+// // Tree.insertNode(84, Tree.root);
 // console.log(prettyPrint(Tree.root));
-// console.log(Tree.find(82, Tree.root));
-console.log("first level", Tree.levelOrder(Tree.root, mult));
-console.log("second level", Tree.levelOrderRecursion(Tree.root));
-console.log(Tree.root);
+// // Tree.deleteNode(50, Tree.root);
+// // console.log(prettyPrint(Tree.root));
+// // console.log(Tree.find(82, Tree.root));
+// // console.log("first level", Tree.levelOrder(Tree.root, mult));
+// // console.log("second level", Tree.levelOrderRecursion(Tree.root));
+// console.log(Tree.root);
 
-console.log("inOrder:", Tree.inOrder(Tree.root, mult));
+// // console.log("inOrder:", Tree.inOrder(Tree.root, mult));
 
-console.log(Tree.root);
-console.log("pre:", Tree.preOrder(Tree.root));
-// console.log("post:", Tree.postORder(Tree.root));
+// console.log(Tree.root);
+// console.log("pre:", Tree.preOrder(Tree.root));
+// // console.log("post:", Tree.postORder(Tree.root));
+
+// // console.log(Tree.height(Tree.root));
+// console.log(Tree.depth(Tree.root, Tree.root.right.right));
+// console.log(Tree.isBalanced(Tree.root));
+// Tree.reBalance(Tree.root);
+// Tree.insertNode(32, Tree.root);
+// Tree.insertNode(33, Tree.root);
+// console.log(prettyPrint(Tree.root));
+// console.log(Tree.isBalanced(Tree.root));
+// console.log(Tree.inOrder(Tree.root));
+// Tree.reBalance(Tree.root);
+// console.log(prettyPrint(Tree.root));
+// console.log(Tree.isBalanced(Tree.root));
+// console.log(prettyPrint(Tree.root));
+// console.log(Tree.depth(Tree.root, Tree.root.right.right.right.right));
+// function mult(node) {
+//   const modifiedNode = node.data * 2;
+//   // node.data = node.data * 2;
+
+//   console.log(node.data * 2);
+//   return modifiedNode;
+// }
+
 // function component() {
 //   const element = document.createElement("div");
 
@@ -51,16 +76,3 @@ console.log("pre:", Tree.preOrder(Tree.root));
 //   arr.push(value);
 //   return arr;
 // }
-
-// console.log(appendToArray(1)); // Output: [1]
-// console.log(appendToArray(2)); // Output: [1, 2]
-
-// let arara = [];
-
-function mult(node) {
-  const modifiedNode = node.data * 2;
-  // node.data = node.data * 2;
-
-  console.log(node.data * 2);
-  return modifiedNode;
-}
